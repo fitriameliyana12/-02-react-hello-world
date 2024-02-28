bc1f94c0e88b12f87e90e90a31d770a948a856db
fitriameliyana12 <fitriameliyana1201@gmail.com>
2024-02-28 11:09:20 +0700

    "W03: Jawaban soal 1"

---
+++ 03-belajar-komponen/my-app/next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    images: {
+      domains: ['dummyimage.com', 'prnt.sc', 'imgur.com', 'i.imgur.com'], // Add any additional domains as needed
+    },
+  };
--- 03-belajar-komponen/my-app/src/components/profile.tsx
+++ 03-belajar-komponen/my-app/src/components/profile.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Profile() {
     return (
       <Image
-        src="https://i.pinimg.com/originals/59/d3/c8/59d3c8b61242743267659cff8084ae8a.jpg"
+        src="https://i.imgur.com/MK3eW3Am.jpg"
         alt="Katherine Johnson"
         width={100}
         height={100}
