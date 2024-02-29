27554225e21de9baada71fde1e1c49a2181eea2c
fitriameliyana12 <fitriameliyana1201@gmail.com>
2024-02-29 12:07:55 +0700

    "W03: Jawaban soal 5"

--- 03-belajar-komponen/my-app/src/app/page.tsx
+++ 03-belajar-komponen/my-app/src/app/page.tsx
@@ -21,16 +21,24 @@ import TodoList from "@/components/todoList";
 
 export default function Home() {
   return (
-    <div>
-      {/* <h1 className="text-center">Ilmuwan yang luar biasa</h1>
-      <div className="grid grid-cols-3 gap-8 justify-center">
-     <Gallery/>
-     </div> */}
-     {/* <Bio/> */}
-     <TodoList/>
+  //   <div>
+  //     {/* <h1 className="text-center">Ilmuwan yang luar biasa</h1>
+  //     <div className="grid grid-cols-3 gap-8 justify-center">
+  //    <Gallery/>
+  //    </div> */}
+  //    {/* <Bio/> */}
+  //    {/* <TodoList/> */}
+  // </div>
 
-     
-  </div>
+  <section>
+      <h1 className="font-semibold text-slate-900 truncate pr-20 text-center">Ilmuwan yang luar biasa</h1>
+      <hr />
+      <Gallery />
+      <hr />
+      <TodoList />
+    </section>
+
+  
     
   );
 }
