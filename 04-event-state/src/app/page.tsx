2d0d4dec668eda860e309c7146c08ea43b9a44c5
fitriameliyana12 <fitriameliyana1201@gmail.com>
2024-03-06 10:49:40 +0700

    04-event-state

---
+++ 04-event-state/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Tombol_1 from "@/components/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="container mx-auto">
+      <h2>Kuis Kota</h2>
+      <Tombol_1/>
+    </div>
+  );
+}
